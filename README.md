ae0fdc10ec86cb99f6a844d325470fc5c776d451
Patrick Kranz <patrick-kranz@live.de>
2025-10-01 11:27:17 +0000

    updated the cardav node

--- README.md
+++ README.md
@@ -1,35 +1,43 @@
 # node-red-contrib-nextcloud
-Collection of node-red nodes to download Calendars (CalDAV) and Contacts
-(CardDAV) and up- / download / list files (WebDAV) from / to [nextcloud](https://nextcloud.com/)
 
-Detailed information can be found in the build in info of each node
+A fork of [Torsten Kühnels node-red-contrib-nextcloud](https://github.com/kuehnelbs/node-red-contrib-nextcloud). 
 
-## Installation
-npm install node-red-contrib-nextcloud
+I updated the cardav node.
 
-### CalDAV
-- based on [dav](https://github.com/lambdabaa/dav) package using CalDAV protocol
-- reads all calendars and sends a message for each calendar
-- reads a specified calandar if set in node properties or incoming message
+It now returns a JSON in the form of:
+```json
+{
+    name: 'the name of the adress book',
+    data:[
+         {
+            "raw": "BEGIN:VCARD\r\nVERSION:3.0\r\n…\r\nEND:VCARD",
+            "VERSION": "3.0",
+            "PRODID": "-//Sabre//Sabre VObject 4.5.6//EN",
+            "lastName": "…",
+            "firstName": "…",
+            "additionalNames": "",
+            "namePrefix": "",
+            "nameSuffix": "",
+            "emailAddresses": {
+                "other": [
+                    "…@….de"
+                ]
+            },
+            "organization": "Some organisation",
+            "phoneNumbers": {
+                "cell": [
+                    "+49 …"
+                ],
+                "work": [
+                    "+49 …"
+                ]
+            },
+            "uid": "…",
+            "fullName": "…"
+        },
+    ]
+}
 
-### CardDAV
-- based on [dav](https://github.com/lambdabaa/dav) package using CalDAV protocol
-- reads all addressbooks and sends a message for each adressbook
-- reads a specified addressbook if set in node properties or incoming message
+```
 
-### WebDAV
-- based on [webdav](https://github.com/perry-mitchell/webdav-client) package using WebDAV protocol
-
-##### Read content of a server directory
-- reads content of the users root directory if no folder is specified
-- reads content of a given directory specified in node properties or incoming message
-
-##### Upload a file to a server directory
-- uploads a file to nextcloud server
-- absolute path of upload file can be set in node properties or incoming message
-- server directory can be set in node properties or incoming message
-
-##### Download a file from a server directory
-- downloads a file from nextcloud server
-- file on server can be set in node properties or incoming message
-- sends file as binary buffer to the output. File can be stored using file node
+The other implementations are unchanged (for now).
--- nextcloud.js
+++ nextcloud.js
@@ -5,8 +5,9 @@ module.exports = function (RED) {
   const IcalExpander = require('ical-expander')
   const moment = require('moment')
   const https = require('https')
+  const tsdav = require('tsdav')
 
-  function NextcloudConfigNode (config) {
+  function NextcloudConfigNode(config) {
     RED.nodes.createNode(this, config)
     this.address = config.address
     this.insecure = config.insecure
@@ -18,7 +19,18 @@ module.exports = function (RED) {
     }
   })
 
-  function NextcloudCalDav (config) {
+  // vCard De-Escape Hilfsfunktion
+  function vcardDeEscape(str) {
+    if (typeof str !== 'string') return str;
+    return str
+      .replace(/\\n/g, '\n')
+      .replace(/\\,/g, ',')
+      .replace(/\\;/g, ';')
+      .replace(/\\\\/g, '\\');
+  }
+
+
+  function NextcloudCalDav(config) {
     RED.nodes.createNode(this, config)
     this.server = RED.nodes.getNode(config.server)
     this.calendar = config.calendar
@@ -90,13 +102,13 @@ module.exports = function (RED) {
         })
     })
 
-    function convertEvents (events) {
+    function convertEvents(events) {
       const mappedEvents = events.events.map(_convertEvent)
       const mappedOccurrences = events.occurrences.map(_convertEvent)
       return [].concat(mappedEvents, mappedOccurrences)
     }
 
-    function _convertEvent (e) {
+    function _convertEvent(e) {
       if (e) {
         let startDate = e.startDate.toString()
         let endDate = e.endDate.toString()
@@ -108,16 +120,26 @@ module.exports = function (RED) {
           delete e.duration.wrappedJSObject
         }
 
+        // vCard De-Escape Hilfsfunktion
+        function vcardDeEscape(str) {
+          if (typeof str !== 'string') return str;
+          return str
+            .replace(/\\n/g, '\n')
+            .replace(/\\,/g, ',')
+            .replace(/\\;/g, ';')
+            .replace(/\\\\/g, '\\');
+        }
+
         return {
           startDate: startDate,
           endDate: endDate,
-          summary: e.summary || '',
-          description: e.description || '',
+          summary: vcardDeEscape(e.summary || ''),
+          description: vcardDeEscape(e.description || ''),
           attendees: e.attendees,
           duration: e.duration.toICALString(),
           durationSeconds: e.duration.toSeconds(),
-          location: e.location || '',
-          organizer: e.organizer || '',
+          location: vcardDeEscape(e.location || ''),
+          organizer: vcardDeEscape(e.organizer || ''),
           uid: e.uid || '',
           isRecurring: false,
           allDay: ((e.duration.toSeconds() % 86400) === 0)
@@ -127,62 +149,232 @@ module.exports = function (RED) {
   }
   RED.nodes.registerType('nextcloud-caldav', NextcloudCalDav)
 
-  function NextcloudCardDav (config) {
+  function NextcloudCardDav(config) {
     RED.nodes.createNode(this, config)
     this.server = RED.nodes.getNode(config.server)
     this.addressBook = config.addressBook
     const node = this
 
-    node.on('input', (msg) => {
-      const xhr = new dav.transport.Basic(
-        new dav.Credentials({
-          username: node.server.credentials.user,
-          password: node.server.credentials.pass
-        })
-      )
+    node.on('input', async (msg) => {
 
-      // Server + Basepath
-      let cardDavUri = node.server.address + '/remote.php/dav/addressbooks/users/'
-      // User
-      cardDavUri += node.server.credentials.user + '/'
-      // ToDo Filter ?
-      dav.createAccount({ server: cardDavUri, xhr: xhr, accountType: 'carddav' })
-        .then(function (account) {
-          if (!account.addressBooks) {
-            node.error('Nextcloud:CardDAV -> no addressbooks found.')
-            return
+      try {
+
+
+        // Server + Basepath
+        let cardDavUri = node.server.address + '/remote.php/dav/addressbooks/'
+        // User
+        // cardDavUri += node.server.credentials.user + '/'
+
+
+
+        const client = await tsdav.createDAVClient({
+          serverUrl: cardDavUri,
+          credentials: {
+            username: node.server.credentials.user,
+            password: node.server.credentials.pass
+          },
+          defaultAccountType: 'carddav',
+          authMethod: 'Basic',
+
+        });
+
+        const addressBooks = await client.fetchAddressBooks({
+        });
+
+
+        // get selected address book
+        let selectedAddressBook = null;
+        let abName = msg.addressBook || node.addressBook;
+        if (abName && abName.length) {
+          selectedAddressBook = addressBooks.find(ab => ab.displayName === abName);
+          if (!selectedAddressBook) {
+            node.error(`Nextcloud:CardDAV -> Address book "${abName}" not found. Available address books: ${addressBooks.map(ab => ab.displayName).join(', ')}`);
+            return;
           }
-          // account instanceof dav.Account
-          account.addressBooks.forEach(function (addressBook) {
-            // Wenn Adressbuch gesetzt ist, dann nur diesen abrufen
-            let c = msg.addressBook || node.addressBook
-            if (!c || !c.length || (c && c.length && c === addressBook.displayName)) {
-              dav.listVCards(addressBook, { xhr: xhr })
-                .then(function (addressBookEntries) {
-                  let vcfList = { 'payload': { 'name': addressBook.displayName, 'data': [] } }
-                  addressBookEntries.forEach(function (addressBookEntry) {
-                    const keyValue = addressBookEntry.addressData.split('\n')
-                    let vcfJson = {}
-                    for (let x = 0; x < keyValue.length; x++) {
-                      const temp = keyValue[x].split(':')
-                      vcfJson[temp[0]] = temp[1]
-                    }
-                    vcfList.payload.data.push(vcfJson)
-                  })
-                  node.send(vcfList)
-                }, function () {
-                  node.error('Nextcloud:CardDAV -> get cards went wrong.')
-                })
-            }
-          })
-        }, function () {
-          node.error('Nextcloud:CardDAV -> get addressBooks went wrong.')
+        } else {
+          // if no address book specified, use the first one
+          selectedAddressBook = addressBooks[0];
+        }
+
+        if (!selectedAddressBook) {
+          node.error('Nextcloud:CardDAV -> No address books found.');
+          return;
+        }
+
+
+
+
+        const cards = await client.fetchVCards({
+          addressBook: selectedAddressBook,
+
         })
+        const vcfList = { 'payload': { 'name': selectedAddressBook.displayName, 'data': [] } };
+        cards.forEach(card => {
+
+
+          const keyValue = card.data.split('\n');
+
+          // vCard line folding: lines starting with space or tab are continuations of previous line
+          const joinedKeyValue = [];
+          for (let i = 0; i < keyValue.length; i++) {
+            const line = keyValue[i];
+            if (/^[ \t]/.test(line)) {
+              // continuation line: remove leading whitespace and append
+              if (joinedKeyValue.length > 0) {
+                joinedKeyValue[joinedKeyValue.length - 1] += line.replace(/^\s+/, '');
+              }
+            } else {
+              joinedKeyValue.push(line);
+            }
+          }
+
+
+          let vcfJson = {};
+          vcfJson['raw'] = card.data;
+          let beginFound = false;
+          let endFound = false;
+          for (let x = 0; x < joinedKeyValue.length; x++) {
+            const [keyRaw, value] = joinedKeyValue[x].split(/:(.*)/)
+              .map(s => s.trim());
+
+            const [key, ...rest] = keyRaw.split(';');
+            const keyCategories = Object.fromEntries(rest.map(part => {
+              const [k, v] = part.split('=');
+              return [k.toLowerCase(), v.toLowerCase()];
+            }));
+
+
+
+            if (key === 'BEGIN' && value === 'VCARD') {
+              beginFound = true;
+              continue;
+            } else if (!beginFound) {
+              // skip all lines until BEGIN:VCARD is found
+              node.warn(`Skipping line before BEGIN:VCARD: key=${key}, value=${value}`);
+              continue;
+            }
+
+            if (key === 'END' && value === 'VCARD') {
+              endFound = true;
+              break;
+            }
+
+            if (key == 'FN') {
+              // full name
+              vcfJson['fullName'] = vcardDeEscape(value);
+            } else if (key == 'N') {
+              // name parts
+              const nameParts = value.split(';');
+              vcfJson['lastName'] = vcardDeEscape(nameParts[0] || '');
+              vcfJson['firstName'] = vcardDeEscape(nameParts[1] || '');
+              vcfJson['additionalNames'] = vcardDeEscape(nameParts[2] || '');
+              vcfJson['namePrefix'] = vcardDeEscape(nameParts[3] || '');
+              vcfJson['nameSuffix'] = vcardDeEscape(nameParts[4] || '');
+            } else if (key == 'TEL' || key.startsWith('TEL;')) {
+              // phone number
+              if (!vcfJson['phoneNumbers']) {
+                vcfJson['phoneNumbers'] = {};
+              }
+              const type = keyCategories['type'] || 'other';
+              if (!vcfJson['phoneNumbers'][type]) {
+                vcfJson['phoneNumbers'][type] = [];
+              }
+              vcfJson['phoneNumbers'][type].push(value);
+            } else if (key == 'EMAIL' || key.startsWith('EMAIL;')) {
+              // email address
+              if (!vcfJson['emailAddresses']) {
+                vcfJson['emailAddresses'] = {};
+              }
+              const type = keyCategories['type'] || 'other';
+              if (!vcfJson['emailAddresses'][type]) {
+                vcfJson['emailAddresses'][type] = [];
+              }
+              vcfJson['emailAddresses'][type].push(value);
+            } else if (key == 'ADR' || key.startsWith('ADR;')) {
+              // address
+              if (!vcfJson['addresses']) {
+                vcfJson['addresses'] = {};
+              }
+              let adrParts = value.split(';');
+              let address = {
+                poBox: vcardDeEscape(adrParts[0] || ''),
+                extendedAddress: vcardDeEscape(adrParts[1] || ''),
+                streetAddress: vcardDeEscape(adrParts[2] || ''),
+                locality: vcardDeEscape(adrParts[3] || ''),
+                region: vcardDeEscape(adrParts[4] || ''),
+                postalCode: vcardDeEscape(adrParts[5] || ''),
+                countryName: vcardDeEscape(adrParts[6] || '')
+              };
+              const type = keyCategories['type'] || 'other';
+              if (!vcfJson['addresses'][type]) {
+                vcfJson['addresses'][type] = [];
+              }
+              vcfJson['addresses'][type].push(address);
+            } else if (key == 'ORG') {
+              vcfJson['organization'] = vcardDeEscape(value);
+            } else if (key == 'TITLE') {
+              vcfJson['jobTitle'] = vcardDeEscape(value);
+            } else if (key == 'URL' || key.startsWith('URL;')) {
+              // url
+              if (!vcfJson['urls']) {
+                vcfJson['urls'] = {};
+              }
+              const type = keyCategories['type'] || 'other';
+              // no type, use 'other'
+              if (!vcfJson['urls'][type]) {
+                vcfJson['urls'][type] = [];
+              }
+              vcfJson['urls'][type].push(value);
+
+            } else if (key == 'NOTE') {
+              vcfJson['note'] = value;
+            } else if (key == 'BDAY') {
+              vcfJson['birthday'] = value;
+            } else if (key == 'REV') {
+              vcfJson['revision'] = value;
+            } else if (key == 'UID') {
+              vcfJson['uid'] = value;
+            } else if (key == 'PHOTO' || key.startsWith('PHOTO;')) {
+              // photo
+              if (/;ENCODING=b;/.test(key)) {
+                // base64 encoded
+                vcfJson['photo'] = value;
+              } else if (/;VALUE=uri;/.test(key)) {
+                vcfJson['photoUrl'] = value;
+              }
+            } else if (vcfJson[key] === undefined) {
+              // other properties
+              vcfJson[key.toLowerCase()] = value;
+            }
+          }
+          if (!endFound) {
+            node.error('END:VCARD not found, skipping entry');
+            node.error(JSON.stringify(card));
+            return;
+          }
+          if (!vcfJson['fullName']) {
+            vcfJson['fullName'] = (vcfJson['firstName'] ?? '') + ' ' + (vcfJson['lastName'] ?? '');
+          }
+
+
+
+
+          vcfList.payload.data.push(vcfJson);
+        });
+        node.send(vcfList);
+      } catch (error) {
+        node.error('Nextcloud:CardDAV -> Error fetching address books: ' + error);
+        // print stack trace
+        console.error(error);
+      }
     })
+
+    return; // Old dav code is deprecated, but left here for reference
   }
+
   RED.nodes.registerType('nextcloud-carddav', NextcloudCardDav)
 
-  function NextcloudWebDavList (config) {
+  function NextcloudWebDavList(config) {
     RED.nodes.createNode(this, config)
     this.server = RED.nodes.getNode(config.server)
     this.directory = config.directory
@@ -214,7 +406,7 @@ module.exports = function (RED) {
   }
   RED.nodes.registerType('nextcloud-webdav-list', NextcloudWebDavList)
 
-  function NextcloudWebDavOut (config) {
+  function NextcloudWebDavOut(config) {
     RED.nodes.createNode(this, config)
     this.server = RED.nodes.getNode(config.server)
     this.filename = config.filename
@@ -249,7 +441,7 @@ module.exports = function (RED) {
   }
   RED.nodes.registerType('nextcloud-webdav-out', NextcloudWebDavOut)
 
-  function NextcloudWebDavIn (config) {
+  function NextcloudWebDavIn(config) {
     RED.nodes.createNode(this, config)
     this.server = RED.nodes.getNode(config.server)
     this.directory = config.directory
